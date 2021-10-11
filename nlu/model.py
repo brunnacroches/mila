b5d1dab5a7fa2c5db82873567ff44b0c7a79ef48
brunnacroches <brunnacroches@gmail.com>
2021-10-11 07:23:00 -0300

    rodando

--- nlu/model.py
+++ nlu/model.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Embedding
 from tensorflow.keras.utils import to_categorical
 
-data = yaml.safe_load(open('nlu\\train.yml', 'r', encoding='utf-8').read())
+data = yaml.safe_load(open('nlu/train.yml', 'r', encoding='utf-8').read())
 
 inputs, outputs = [], []
 
@@ -16,26 +16,8 @@ for command in data['commands']:
 
 # Processar texto: palavras, caracteres, bytes, sub-palavras
 
-chars = set()
+max_seq = max([len(bytes(x.encode('utf-8'))) for x in inputs])
 
-for input in inputs + outputs:
-    for ch in input:
-        if ch not in chars:
-            chars.add(ch)
-
-# Mapear char-idx
-
-chr2idx = {}
-idx2chr = {}
-
-for i, ch in enumerate(chars):
-    chr2idx[ch] = i
-    idx2chr[i] = ch
-
-
-max_seq = max([len(x) for x in inputs])
-
-print('Número de chars:', len(chars))
 print('Maior seq:', max_seq)
 
 # Criar dataset one-hot (número de examplos, tamanho da seq, num caracteres)
@@ -43,20 +25,19 @@ print('Maior seq:', max_seq)
 
 # Input Data one-hot encoding
 
-input_data = np.zeros((len(inputs), max_seq, len(chars)), dtype='int32')
-for i, input in enumerate(inputs):
-    for k, ch in enumerate(input):
-        input_data[i, k, chr2idx[ch]] = 1.0
-
+input_data = np.zeros((len(inputs), max_seq, 256), dtype='float32')
+for i, inp in enumerate(inputs):
+    for k, ch in enumerate(bytes(inp.encode('utf-8'))):
+        input_data[i, k, int(ch)] = 1.0
 
 
 # Input data sparse
-
+'''
 input_data = np.zeros((len(inputs), max_seq), dtype='int32')
-
 for i, input in enumerate(inputs):
     for k, ch in enumerate(input):
         input_data[i, k] = chr2idx[ch]
+'''
 
 # Output Data
 
@@ -76,20 +57,31 @@ for output in outputs:
 
 output_data = to_categorical(output_data, len(output_data))
 
- 
+
 print(output_data[0])
 
 model = Sequential()
-model.add(Embedding(len(chars), 64))
-model.add(LSTM(128, return_sequences=True))
+model.add(LSTM(128))
 model.add(Dense(len(output_data), activation='softmax'))
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
 
-model.fit(input_data, output_data, epochs=16)
+model.fit(input_data, output_data, epochs=128)
 
+# Classificar texto em um entidade
+def classify(text):
+    # Criar um array de entrada
+    x = np.zeros((1, 48, 256), dtype='float32')
 
-'''
-print(inputs)
-print(outputs)
-'''
+    # Preencher o array com dados do texto.
+    for k, ch in enumerate(bytes(text.encode('utf-8'))):
+        x[0, k, int(ch)] = 1.0
+
+    # Fazer a previsão
+    out = model.predict(x)
+    idx = out.argmax()
+    print(idx2label[idx])
+
+while True:
+    text = input('Digite algo: ')
+    classify(text)
